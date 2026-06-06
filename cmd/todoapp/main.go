6e26b0f8e02e8043e4bc6a9a2a905bdf08b81327
jetty <swin4ukov.saski@gmail.com>
2026-06-06 23:58:23 +0700

    fixes

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"time"
 
+	core_config "github.com/jettyjunk/goland-todoapp/internal/config"
 	core_logger "github.com/jettyjunk/goland-todoapp/internal/core/logger"
 	core_pgx_pool "github.com/jettyjunk/goland-todoapp/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/jettyjunk/goland-todoapp/internal/core/transport/http/middleware"
@@ -21,12 +22,9 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	timeZone = time.UTC
-)
-
 func main() {
-	time.Local = timeZone
+	cfg := core_config.NewConfigMust()
+	time.Local = cfg.TimeZone
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
