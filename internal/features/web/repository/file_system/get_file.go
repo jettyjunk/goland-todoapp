ad460b57aa30f9ebd428e062bb246760bade6415
jetty <swin4ukov.saski@gmail.com>
2026-06-12 02:54:53 +0700

    add frontend

---
+++ internal/features/web/repository/file_system/get_file.go
@@ -0,0 +1,21 @@
+package web_filesystem_repository
+
+import (
+	"fmt"
+	"os"
+
+	core_errors "github.com/jettyjunk/goland-todoapp/internal/core/errors"
+)
+
+func (r *WebRepository) GetFile(filePath string) ([]byte, error) {
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("file: %s: %w", filePath, core_errors.ErrNotFounde)
+		}
+
+		return nil, fmt.Errorf("get file: %s: %w", file, err)
+	}
+
+	return file, nil
+}
